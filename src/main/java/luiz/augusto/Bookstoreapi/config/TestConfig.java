c92100a0f03e44926affa5f92b733bfe30fdd9ec
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-07-17 23:08:59 -0300

    Adding spring security and mysql database

--- src/main/java/luiz/augusto/Bookstoreapi/config/TestConfig.java
+++ src/main/java/luiz/augusto/Bookstoreapi/config/TestConfig.java
@@ -1,9 +1,22 @@
 package luiz.augusto.Bookstoreapi.config;
 
+import lombok.RequiredArgsConstructor;
+import luiz.augusto.Bookstoreapi.database.DatabaseInit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("test")
 @Configuration
+@RequiredArgsConstructor
 public class TestConfig {
+
+    private final DatabaseInit databaseInit;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+    @Bean
+    public void initDatabase(){
+        if(ddlAuto.equals("create")) databaseInit.createObjectsForTesting();
+    }
 }
